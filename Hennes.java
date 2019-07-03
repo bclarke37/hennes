820ad0bf83e824bfd7999345ae9a7cad3bb1f442
Bruce <43864468+bclarke37@users.noreply.github.com>
2019-07-03 14:42:05 -0700

    Add files via upload

--- Hennes.java
+++ Hennes.java
@@ -6,7 +6,9 @@ public class Hennes {
 
    public static void main(String[] args) throws FileNotFoundException, IOException{
       File f = new File("log.txt");
-      Scanner view = new Scanner(f);
+      File g = new File("log.txt");
+      Scanner hour = new Scanner(f);
+      Scanner mood = new Scanner(f);
       Scanner console = new Scanner(System.in);
             
       getDate d = new getDate();
@@ -14,10 +16,9 @@ public class Hennes {
       FileWriter output = new FileWriter("C:/Users/Bruce/Documents/University of Washington (UW) Year 1/Spring Quarter/CSE142/HMprogram/log.txt", true); //Set true for append mode
       PrintWriter log = new PrintWriter(output);
       
+      int[] moodLog = moodArray(mood);
       int[] shiftCount = new int[1];
-      int cumHours = cumHours(shiftCount, view);
-//       int[] hoursLog = new int[1];
-      int[] moodLog = moodArray(view);
+      int cumHours = cumHours(shiftCount, hour);
       
       log.println(d.date());
       System.out.println("Hello, Bruce. Today is " + d.date());
@@ -32,7 +33,7 @@ public class Hennes {
          if (answer.equalsIgnoreCase("c")) {
             create(console, log, cumHours);
          } else if (answer.equalsIgnoreCase("v")) {
-            view(console, view, cumHours);
+            view(console, cumHours, moodLog, shiftCount);
          }
          System.out.println();
          System.out.println("(C)reate new entry :: (V)iew logs :: (E)nd");
@@ -43,6 +44,7 @@ public class Hennes {
       }
       
       System.out.println();
+      System.out.println();
       System.out.println("   See you next time! :D");
    }
    
@@ -51,7 +53,7 @@ public class Hennes {
 
       System.out.print("How many hours did you work today? ");
       int hoursWorkedToday = console.nextInt();
-      log.println("     Hours: " + hoursWorkedToday + "(" + (cumHours + hoursWorkedToday) + " total)");
+      log.println("     Hours: " + hoursWorkedToday + " (" + (cumHours + hoursWorkedToday) + " total)");
       
       System.out.print("What was work like today? (Enter 1-4) ");
       int mood = console.nextInt();
@@ -68,7 +70,7 @@ public class Hennes {
          System.out.print("Aww, looks like the day didn't go too well :( What happened? ");
       } else if (mood == 3) {
          log.println("      Mood: Good!");
-         System.out.print("What made today good? ");
+         System.out.print("What do you want to say about today? ");
       } else {
          log.println("      Mood: Amazing!");
          System.out.print("Wow! Today must have been awesome! What happened? ");
@@ -79,35 +81,34 @@ public class Hennes {
       log.println("  Comments: " + comment);
       log.println("---");
       log.close();
+      System.out.println("Entry recorded, thank you!");
    }
    
-   public static int cumHours(int[] shiftCount, Scanner view) {
+   public static int cumHours(int[] shiftCount, Scanner hour) {
       int cumHours = 0;
       
-      while (view.hasNext()) {
-         if (view.next().equals("Hours:")) {
-            cumHours += view.nextInt();
+      while (hour.hasNext()) {
+         if (hour.next().equals("Hours:")) {
+            cumHours += hour.nextInt();
             shiftCount[0]++;
          }
       }
       return cumHours;
    }
    
-   public static int[] moodArray(Scanner view) {
+   public static int[] moodArray(Scanner mood) {
       int[] moodLog = new int[4];
    
-      while (view.hasNext()) {
-         if (view.Next().equals("Mood:")) {
-            if (view.Next().equals("Amazing!")) {
+      while (mood.hasNext()) {
+         if (mood.next().equals("Mood:")) {
+            String moodValue = mood.next();
+            if (moodValue.equals("Amazing!")) {
                moodLog[0]++;
-            }
-            if (view.Next().equals("Good!")) {
+            } else if (moodValue.equals("Good!")) {
                moodLog[1]++;
-            }
-            if (view.Next().equals("Meh")) {
+            } else if (moodValue.equals("Meh")) {
                moodLog[2]++;
-            }
-            if (view.Next().equals("Terrible...")) {
+            } else { // (moodValue.equals("Terrible..."))
                moodLog[3]++;
             }
          }
@@ -119,20 +120,18 @@ public class Hennes {
       System.out.println();
       System.out.println("What would you like to view?");
       System.out.println();
-      System.out.println("(O)verall log");
-      System.out.println("(M)ood log");
-      System.out.println("(H)ours & Money log");
+      System.out.println(" (M)ood log");
+      System.out.println(" (H)ours & Money log");
       System.out.println();
-      System.out.println("(G)o back");
+      System.out.println();
+      System.out.println("(G)o back to main menu");
       
       String answer = console.nextLine();
       return answer;
    }
    
    
-   public static void view(Scanner console, Scanner view, int cumHours) throws FileNotFoundException {
-//       double avgHours = (double) hoursLog[0] / shiftCount[0];
-//       double moneyMade = (double) hoursLog[0] * 12.50;
+   public static void view(Scanner console, int cumHours, int[] moodLog, int[] shiftCount) throws FileNotFoundException {
       
       String answer = viewMenu(console);
       System.out.println();
@@ -140,37 +139,32 @@ public class Hennes {
       while (!answer.equalsIgnoreCase("g")) {
          while (!answer.equalsIgnoreCase("d")) {
          
-            if (answer.equalsIgnoreCase("o")) {
-               System.out.println();
-               
-               while (view.hasNextLine()) {
-                  System.out.println(view.nextLine());
-               }
-               
-               System.out.print("   Type 'd' when done");
-               answer = console.nextLine();
-               
-            } else if (answer.equalsIgnoreCase("m")) {
-                  System.out.println("   Mood Log");
-                  System.out.println("Number of 'amazing' days: ");
-                  System.out.println("Number of 'good' days: ");
-                  System.out.println("Number of 'meh' days: ");
-                  System.out.println("Number of 'bad' days: ");
-                  
-                  System.out.print("   Type 'd' when done ");
+            if (answer.equalsIgnoreCase("m")) {
+                  System.out.println("  Mood Log");
+                  System.out.println("Number of 'amazing' days: " + moodLog[0]);
+                  System.out.println("Number of 'good' days: " + moodLog[1]);
+                  System.out.println("Number of 'meh' days: " + moodLog[2]);
+                  System.out.println("Number of 'bad' days: " + moodLog[3]);
+                  System.out.println();
+                  System.out.print("   Type 'd' when done: ");
+                  answer = console.nextLine();
+            } else if (answer.equalsIgnoreCase("h")) {
+                  System.out.println("  Hours");
+                  System.out.println(" Total hours worked: " + cumHours);
+                  System.out.println("Total shifts worked: " + shiftCount[0]);
+                  System.out.println("Average hours worked per shift: " + (double) cumHours / shiftCount[0]);
+                  System.out.println();
+                  System.out.println("Total Money Made: $" + cumHours * 12.50);
+                  System.out.println();
+                  System.out.print("   Type 'd' when done: ");
                   answer = console.nextLine();
             } else {
-                  System.out.println("   Hours");
-                  System.out.println("Total hours: ");
-                  System.out.println("Average hours worked: ");
-                  System.out.println();
-                  System.out.println("Total Money Made: $");
-                  
-                  System.out.print("   Type 'd' when done ");
-                  answer = console.nextLine();
+               System.out.print("Please type either 'm', 'h', or 'g': ");
+               answer = console.nextLine();
             }
          }
          answer = viewMenu(console);
       }
    }
+   
 }
